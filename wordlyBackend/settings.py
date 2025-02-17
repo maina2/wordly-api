969d1b9ddc8f1f14f810ddf20ebf824a9e00bc80
Simon Chanzu <chanzusimon6@gmail.com>
2025-02-17 17:33:15 +0300

    uploads today

--- wordlyBackend/settings.py
+++ wordlyBackend/settings.py
@@ -129,6 +129,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS settings
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://wordly-client.onrender.com",  # Production URL for your React frontend
+    "http://localhost:3000",               # Localhost URL for React development
+]
 
 # Media files
 MEDIA_URL = "/media/"
