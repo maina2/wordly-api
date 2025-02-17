8da3d885a04ac98937cde727616086026c27e819
Simon Chanzu <chanzusimon6@gmail.com>
2025-02-17 14:01:21 +0300

    uploads

--- wordlyBackend/settings.py
+++ wordlyBackend/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-vbskdtq)p1fzn=70l^u+6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS','wordly-api.onrender.com' ,default='localhost,127.0.0.1').split(',')
 
 # Application definition
 INSTALLED_APPS = [
